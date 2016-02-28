f43b4332d67b18eea67a35b728f9520c0ff23121
George Codat Tudoran <office@solcreation.ro>
2016-02-28 20:12:18 +0200

    core of the app

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response) {
+	//throw 'Error' //simulate an error
 	response.send('OK');
 });
 
-app.listen(3000);
+//app.listen(3000);
+module.exports = app;
