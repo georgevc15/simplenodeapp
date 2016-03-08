fbd95e33fa367fd99f7fb6e3211c3fe88cac0d8d
George Codat Tudoran <office@solcreation.ro>
2016-03-08 09:47:48 +0200

    Added Procfile

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var app = express();
 
+/*
 app.get('/', function(request, response) {
 	//throw 'Error' //simulate an error
 	response.send('OK');
+});
+*/
+
+app.use(express.static('public'));
+
+app.get('/cities', function(request, response) {
+	//response.json('OK');
+	var cities =  ['Lotopiax','Caspiana','Indigo'];
+	response.json(cities);
 });
 
 //app.listen(3000);
