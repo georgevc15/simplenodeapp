bc801097eece3f8306b254135f9dec1a42f81b7a
George Codat Tudoran <office@solcreation.ro>
2016-03-08 10:03:46 +0200

    Listen to ports

--- bin/www.js
+++ bin/www.js
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
 
 var app = require('./../app');
+/*
 var port = process.env.PORT || 3000;
 app.listen(3000, function() {
 	console.log('Listening on port' + port);
+});
+*/
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Listening on 3000');
 });
