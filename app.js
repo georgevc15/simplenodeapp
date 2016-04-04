3d16634e6a2bfd05f95b39aca2714e6bb151c159
George Codat Tudoran <office@solcreation.ro>
2016-04-04 12:48:38 +0300

    No select for production

--- app.js
+++ app.js
@@ -19,10 +19,8 @@ if (process.env.REDISTOGO_URL) {
 	client.auth(rtg.auth.split(":")[1]);
 } else {
 	var client = redis.createClient();
+	client.select((process.env.NODE_ENV || 'development').length);
 }
-
-
-client.select((process.env.NODE_ENV || 'development').length);
 //end redis connection
 /*
 client.hset('cities', 'Lotopiax', 'description');
